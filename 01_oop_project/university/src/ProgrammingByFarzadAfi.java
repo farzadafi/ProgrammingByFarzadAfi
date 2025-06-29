416876f11e1bafb5f5e65a5ea427b086d2528ec3
farzadafi <farzadafi40@gmail.com>
2025-06-29 20:56:57 +0330

    add some new :)

--- 01_oop_project/university/src/ProgrammingByFarzadAfi.java
+++
@@ -1,8 +0,0 @@
-import menu.GeneralMenu;
-
-public class ProgrammingByFarzadAfi {
-    public static void main(String[] args) {
-        GeneralMenu generalMenu = new GeneralMenu();
-        generalMenu.generalMenu();
-    }
-}
