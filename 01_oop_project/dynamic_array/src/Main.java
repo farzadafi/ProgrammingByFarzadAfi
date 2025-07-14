9d3c5277b2d8d8170d8e3d0f29e2f105033c61dc
farzadafi <farzadafi40@gmail.com>
2025-07-14 16:10:10 +0330

    step 2

---
+++ 01_oop_project/dynamic_array/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        PersonArray personArray = new PersonArray();
+        personArray.add(new Person());
+    }
+}
--- 01_oop_project/dynamic_array/src/PersonArray.java
+++ 01_oop_project/dynamic_array/src/PersonArray.java
@@ -4,4 +4,13 @@ public class PersonArray {
     public PersonArray() {
         people = new Person[10];
     }
+
+    public void add(Person person) {
+        for (int i = 0; i < people.length; i++) {
+            if(people[i] == null){
+                people[i] = person;
+                break;
+            }
+        }
+    }
 }
