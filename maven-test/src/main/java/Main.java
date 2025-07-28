96adc5e66850efb66131c95deccafec099e3a820
farzadafi <farzadafi40@gmail.com>
2025-07-28 13:58:06 +0330

    create new project for maven-test

---
+++ maven-test/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main{
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		String name = "farzad";
+		String capitalName = StringUtils.capitalize(name);
+		System.out.println(capitalName);
+	}
+}
