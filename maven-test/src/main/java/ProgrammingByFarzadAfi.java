45dcf91800e819bf2ec927c3920db231ad79c8e9
farzadafi <farzadafi40@gmail.com>
2025-07-31 12:50:01 +0330

    change name :)

--- maven-test/src/main/java/ProgrammingByFarzadAfi.java
+++ maven-test/src/main/java/ProgrammingByFarzadAfi.java
@@ -1,8 +1,8 @@
 import org.apache.commons.lang3.StringUtils;
 
-public class Main{
+public class ProgrammingByFarzadAfi {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("ProgrammingByFarzadAfi");
 		String name = "farzad";
 		String capitalName = StringUtils.capitalize(name);
 		System.out.println(capitalName);
