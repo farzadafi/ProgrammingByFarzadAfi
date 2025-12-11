926a143739552906d9c87f19140b0101a6e40162
farzadafi <farzadafi40@gmail.com>
2025-12-11 15:39:03 +0330

    step 03

---
+++ 05_generic/warehouse/src/model/Warehouse.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Warehouse {
+
+    private Box<Book> bookBox;
+    private Box<Phone> phoneBox;
+    private Box<Clothing> clothingBox;
+
+    private void storeBook(Book book) {
+        this.bookBox = new Box<>(book);
+    }
+
+    private void storePhone(Phone phone) {
+        this.phoneBox = new Box<>(phone);
+    }
+
+    private void storeClothing(Clothing clothing) {
+        this.clothingBox = new Box<>(clothing);
+    }
+
+    public void printInventory() {
+        System.out.println("Warehouse Inventory:");
+
+        if (bookBox != null)
+            System.out.println("book=" + bookBox);
+        else
+            System.out.println("book is empty");
+
+        if (phoneBox != null)
+            System.out.println("phone=" + phoneBox);
+        else
+            System.out.println("phone is empty");
+
+        if (clothingBox != null)
+            System.out.println("clothing=" + clothingBox);
+        else
+            System.out.println("clothing is empty");
+
+        System.out.println("==================================");
+    }
+}
