5319244d89e1eb77d28d1e08d404dac1af3967b8
farzadafi <farzadafi40@gmail.com>
2026-02-11 20:17:24 +0330

    add some code for test crud

--- 06_orm/01_orm_tutorial/src/main/java/ir/farzadafi/ProgrammingByFarzadAfi.java
+++ 06_orm/01_orm_tutorial/src/main/java/ir/farzadafi/ProgrammingByFarzadAfi.java
@@ -1,11 +1,42 @@
 package ir.farzadafi;
 
+import ir.farzadafi.model.Student;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class ProgrammingByFarzadAfi {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         System.out.println("Test");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        // CREATE OPERATION
+//        Student ali = new Student(1, "ali", 20);
+//        EntityTransaction transaction = entityManager.getTransaction();
+//        transaction.begin();
+//        entityManager.persist(ali);
+//        transaction.commit();
+
+        //READ OPERATION
+//        Student student = entityManager.find(Student.class, 1);
+//        System.out.println(student);
+
+        //UPDATE OPERATION
+//        Student student = entityManager.find(Student.class, 1);
+//        student.setAge(25);
+//        EntityTransaction transaction = entityManager.getTransaction();
+//        transaction.begin();
+//        entityManager.merge(student);
+//        transaction.commit();
+
+        //DELETE OPERATION
+//        Student student = entityManager.find(Student.class, 1);
+//        EntityTransaction transaction = entityManager.getTransaction();
+//        transaction.begin();
+//        entityManager.remove(student);
+//        transaction.commit();
     }
 }
